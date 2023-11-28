92e537737dfc50644c9f83df2c8bf747aff55b67
BigMike64 <kingmikeike2016@gmail.com>
2023-11-27 20:07:03 -0500

    Update projectJS.js

--- FinalProject/projectJS.js
+++ FinalProject/projectJS.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+    var generateMealPlanBtn = document.getElementById('generateMealPlanBtn');
+    var downloadFormBtn = document.getElementById('downloadFormBtn');
+    var clearFormBtn = document.getElementById('clearFormBtn');
 });
